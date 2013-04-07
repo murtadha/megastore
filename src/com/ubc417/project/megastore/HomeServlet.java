4ea0d5dae2b01cb937be4fa301a25a1151a795a1
samuelsaumanchan <samuelsaumanchan@gmail.com>
2013-04-07 02:09:22 -0700

    removed un-used imports from HomeServlet

--- src/com/ubc417/project/megastore/HomeServlet.java
+++ src/com/ubc417/project/megastore/HomeServlet.java
@@ -1,9 +1,6 @@
 package com.ubc417.project.megastore;
 
 import java.io.IOException;
-import java.util.Date;
-import java.util.Iterator;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -13,9 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.Key;
 import com.ubc417.project.megastore.data.Auctions;
-import com.ubc417.project.megastore.data.Users;
 
 @SuppressWarnings("serial")
 public class HomeServlet extends HttpServlet{
