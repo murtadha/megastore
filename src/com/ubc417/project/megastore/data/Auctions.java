ee4b73c086259ed5c12a1feb654b3c8165dddf1a
samuelsaumanchan <samuelsaumanchan@gmail.com>
2013-04-06 21:34:06 -0700

    Auctions data model implemented Bids data model implemented

--- src/com/ubc417/project/megastore/data/Auctions.java
+++ src/com/ubc417/project/megastore/data/Auctions.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import com.google.appengine.api.datastore.*;
 
 public class Auctions {
-	public static Entity CreateItem(String name, 
+	public static Entity createAuction(String name, 
 			String owner, 
 			String startTime, 
 			String endTime,
@@ -27,7 +27,7 @@ public class Auctions {
 	
 	public static ArrayList<Entity> searchAuctions(String searchString){
 		DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
-		Query query = new Query("Item");
+		Query query = new Query("Auction");
 		
 		Iterable<Entity> allItems = datastoreService.prepare(query).asIterable();
 		ArrayList<Entity> returnedItems = null;
@@ -44,13 +44,30 @@ public class Auctions {
 		
 	}
 	
-	//helper function
-	//DESCRIPTION:TODO
+	public static ArrayList<Entity> getCurrentUserAuctions(String owner){
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		Query query = new Query("Auction");
+		
+		Iterable<Entity> allItems = ds.prepare(query).asIterable();
+		ArrayList<Entity> returnedItems = null;
+		
+		for(Entity currentItem : allItems){
+			if(currentItem.getProperty("owner").toString().contains(owner)){
+				extracted(returnedItems).add(currentItem);
+			
+			}
+			
+		}
+		
+		return extracted(returnedItems);
+	}
+
 	private static ArrayList<Entity> extracted(ArrayList<Entity> returnedItems) {
 		return returnedItems;
 		
 	}
 	
+	//TODO
 	public static Iterable<Entity> getItemRecommendations(String username){
 		return null;
 		
--- src/com/ubc417/project/megastore/data/Bids.java
+++ src/com/ubc417/project/megastore/data/Bids.java
@@ -1,22 +1,66 @@
 package com.ubc417.project.megastore.data;
 
+import java.util.ArrayList;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Query;
 
 public class Bids {
-	public static Entity CreateBid(String itemBidOn, 
-			String bidderUsername, 
-			int bidPrice){
-		Entity bidEntity = new Entity("Bid");
-		bidEntity.setProperty("itemBidOn", itemBidOn);
-		bidEntity.setProperty("bidder", bidderUsername);
-		bidEntity.setProperty("bidPrice", bidPrice);
+	public static Entity createBid(String item, 
+			String bidder, 
+			int price,
+			Key auctionKey){
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();	
 		
-		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		Entity bidEntity = new Entity("Bid", auctionKey);
+		bidEntity.setProperty("item", item);
+		bidEntity.setProperty("bidder", bidder);
+		bidEntity.setProperty("price", price);
+		
 		ds.put(bidEntity);
 		
 		return bidEntity;
 		
 	}
+	
+	public static Boolean deleteBid(Key bidKey){
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		
+		if(bidKey != null){
+			ds.delete(bidKey);
+			return true;
+			
+		} else {
+			return false;
+			
+		}
+		
+	}
+	
+	public static Iterable<Entity> getBidsForUser(String username){
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		Query q = new Query("Bids");
+		
+		Iterable<Entity> allBids = ds.prepare(q).asIterable();
+		ArrayList<Entity> returnedBids  = null;
+		
+		for(Entity currentBid : allBids){
+			if(currentBid.getProperty("name").toString().contains(username)){
+				extracted(returnedBids).add(currentBid);
+			
+			}
+			
+		}
+		
+		return extracted(returnedBids);
+	}
+	
+	private static ArrayList<Entity> extracted(ArrayList<Entity> returnedItems) {
+		return returnedItems;
+		
+	}
+	
 }
