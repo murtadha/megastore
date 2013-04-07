fd29d17344494d14a6d5b25234961c2195cb8576
samuelsaumanchan <samuelsaumanchan@gmail.com>
2013-04-06 21:40:30 -0700

    Ratings data model implemented

--- src/com/ubc417/project/megastore/data/Ratings.java
+++ src/com/ubc417/project/megastore/data/Ratings.java
@@ -1,21 +1,48 @@
 package com.ubc417.project.megastore.data;
 
+import java.util.ArrayList;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Query;
 
 public class Ratings {
-	public static Entity CreateRating(String userBeingRated,
-			String userDoingRating,
-			String rating){
+	public static Entity createRating(String target,
+			String ragter,
+			String value){
 		Entity ratingEntity = new Entity("Rating");
-		ratingEntity.setProperty("userBeingRated", userBeingRated);
-		ratingEntity.setProperty("userDoingRating", userDoingRating);
-		ratingEntity.setProperty("rating", rating);
+		ratingEntity.setProperty("userBeingRated", target);
+		ratingEntity.setProperty("userDoingRating", ragter);
+		ratingEntity.setProperty("rating", value);
 		
 		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
 		ds.put(ratingEntity);
 		
 		return ratingEntity;
+		
 	}
+	
+	public static Iterable<Entity> getRatingsForUser(String username){
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		Query q = new Query("Rating");
+		Iterable<Entity> allRatings = ds.prepare(q).asIterable();
+		ArrayList<Entity> ratingsForUser = null;
+		
+		for(Entity ratingEntity : allRatings){
+			if(ratingEntity.getProperty("username").toString().contains(username)){
+				extracted(ratingsForUser).add(ratingEntity);
+				
+			}
+			
+		}
+		
+		return allRatings;
+		
+	}
+
+	private static ArrayList<Entity> extracted(ArrayList<Entity> ratingsForUser) {
+		return ratingsForUser;
+	}
+	
 }
